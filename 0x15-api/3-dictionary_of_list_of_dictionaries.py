09a4bf3c643c57c91ac2bdbbb92c7a28c3c35fc5
luismelendez94 <2709@holbertonschool.com>
2021-09-14 04:16:39 +0000

    Task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Extend last Python script to export data in the JSON format """
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    employees = requests.get(
+            "https://jsonplaceholder.typicode.com/users/").json()
+    todosDict = {}
+
+    for emp in employees:
+        empID = emp['id']
+        username = emp['username']
+
+        tasks = requests.get(
+                "https://jsonplaceholder.typicode.com/todos?userId={}"
+                .format(empID)).json()
+
+        empTasks = []
+        for task in tasks:
+            task = {"username": username, "task": task['title'],
+                    "completed": task['completed']}
+            empTasks.append(task)
+
+        todosDict[empID] = empTasks
+
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(todosDict, json_file)
