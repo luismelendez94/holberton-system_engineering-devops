5660e96cc7eae98f83df7c91385c654a94159e4b
luismelendez94 <2709@holbertonschool.com>
2021-09-14 00:47:21 +0000

    Requirements

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,26 +4,27 @@
 import requests
 import sys
 
-empID = sys.argv[1]
+if __name__ == "__main__":
+    empID = sys.argv[1]
 
-response = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(empID))
-resp_Todos = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".format(empID))
+    response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                            .format(empID))
+    resp_Todos = requests.get(
+            "https://jsonplaceholder.typicode.com/todos?userId={}"
+            .format(empID))
 
-obj_resp = response.json()
-obj_Todos = resp_Todos.json()
+    obj_resp = response.json()
+    obj_Todos = resp_Todos.json()
 
+    numbTask = 0
+    numbTaskDone = 0
+    taskDone = ""
+    for task in obj_Todos:
+        if task.get('completed'):
+            numbTaskDone += 1
+            taskDone += "\t " + task.get('title') + "\n"
+        numbTask += 1
 
-numbTask = 0
-numbTaskDone = 0
-taskDone = ""
-for task in obj_Todos:
-    if task['completed']:
-        numbTaskDone += 1
-        taskDone += "\t " + task['title'] + "\n"
-    numbTask += 1
-
-print("Employee {} is done with tasks ({}/{}):".format(obj_resp['name'],
-      numbTaskDone, numbTask))
-print(taskDone[:-1])
+    print("Employee {} is done with tasks ({}/{}):"
+          .format(obj_resp.get('name'), numbTaskDone, numbTask))
+    print(taskDone[:-1])
