907a2c31d53bebece30af6fe84931d6c163de399
luismelendez94 <2709@holbertonschool.com>
2021-09-29 03:56:32 +0000

    Fixes

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,13 +7,4 @@ import requests
 
 def recursive(subreddit, hot_list=[]):
     """ Get the total of titles recursively """
-    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
-    headers = {'user-agent': 'my-app/0.0.1'}
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 404:
-        return 0
-    else:
-        print(response.status_code)
-        return 1
+    return None
