b6fba2bfc1b4f6844745845f9c8771deae254f16
luismelendez94 <2709@holbertonschool.com>
2021-09-30 21:44:47 +0000

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,4 +6,18 @@ import requests
 
 def top_ten(subreddit):
     """ Get the total of subs """
-    return "OK"
+
+    url = 'https://www.reddit.com/r/' + subreddit + '/.json?limit=10'
+    headers = 'Mozilla/5.0 (X11; Linux x86_64) ' \
+              'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+              'Chrome/80.0.3987.87 Safari/537.36'
+
+    resp = requests.get(url, allow_redirects=False,
+                        headers={'User-Agent': headers})
+
+    if resp.status_code != 200:
+        print('None')
+    else:
+        resp = resp.json().get('data')
+        for chill in resp.get('children'):
+            print(chill.get('data').get('title'))
