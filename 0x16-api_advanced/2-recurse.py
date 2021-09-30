508a63de19a5524a5f0b0d2cd5d2e4d675949b4b
luismelendez94 <2709@holbertonschool.com>
2021-09-30 22:28:11 +0000

    Task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,6 +5,32 @@
 import requests
 
 
-def recursive(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after="", count=0):
     """ Get the total of titles recursively """
-    return None
+
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot/.json'
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    parameters = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+
+    resp = requests.get(url, allow_redirects=False,
+                        headers=headers, params=parameters)
+
+    if resp.status_code != 200:
+        return None
+    else:
+        result = resp.json().get('data')
+        after = result.get('after')
+        count += result.get('dist')
+
+        for chill in result.get('children'):
+            hot_list.append(chill.get('data').get('title'))
+
+        if after is not None:
+            return recurse(subreddit, hot_list, after, count)
+
+        return hot_list
