152c0875189dedf6231e4fc12cb544a663b486fc
luismelendez94 <2709@holbertonschool.com>
2021-10-04 14:39:43 +0000

    Task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,11 @@
 #Creates Sets up client SSH configuration file
 file_line { 'ssh_config':
-    ensure   => present,
+    ensure   => 'present',
     path     => '/etc/ssh/ssh_config',
     line     => 'PasswordAuthentication no',
-    multiple => 'true'
 }
 file_line { 'ssh_config_2':
-    ensure   => present,
+    ensure   => 'present',
     path     => '/etc/ssh/ssh_config',
     line     => 'IdentityFile ~/.ssh/holberton',
-    multiple => 'true'
 }
